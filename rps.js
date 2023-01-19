2aab829456e826c293d387963c641a53fca57c5c
Daniel <danieljsinger9@gmail.com>
2023-01-19 00:11:57 -0800

    finished rps

--- rps.js
+++ rps.js
@@ -1,4 +1,4 @@
-const array = ["rock" , "paper" , "scissors"];
+let array = ["rock" , "paper" , "scissors"];
 
 function getComputerChoice() {
 
@@ -12,6 +12,9 @@ return item;
 
 const result = getComputerChoice();
 
+/* increment (scoreboardA = 0, i < 0, i++) {
+    s1++;
+ } */
 
 function playRound(playerSelectionArray, computerSelection) {
 
@@ -24,13 +27,93 @@ function playRound(playerSelectionArray, computerSelection) {
     } else if (playerSelectionArray === "scissors" && computerSelection === "rock") {
         return "Computer Wins! Rock beats Scissors";
     } else {
-        return "No game, Please choose rock paper or scissors";
+        
+        return "You Win!";
+    }
+
+}
+let scoreboardA = 0;
+
+let scoreboardB = 0;
+
+const sA = document.querySelector('#scoreboardA');
+
+const sB = document.querySelector('#scoreboardB');
+
+const btnR = document.getElementById('btnR');
+
+const btnS = document.getElementById('btnS');
+
+const btnP = document.getElementById('btnP');
+
+let playerScore = 0;
+
+let computerScore = 0;
+
+function userWin() {
+    if (playerScore === 5) {
+        alert("Player Wins!")
+    } else if (computerScore === 5) {
+        alert("Computer Wins!")
+    } else {
+        return null;
     }
 }
 
-function game() {
+btnR.addEventListener('click', () => {
+
+    let result = playRound (`${playerSelectionArray.at(0)}` , getComputerChoice())
+    if (result === "You Win!") {
+        playerScore++;
+        sA.innerText = playerScore;
+        
+    } else if (result === "It's a Tie!") {
+        return null;
+    } else {
+        computerScore++;
+        sB.innerText = computerScore; 
+    }
+    userWin();
+});
+
+
+btnP.addEventListener('click', () => {
+
+    let result = playRound (`${playerSelectionArray.at(0)}` , getComputerChoice())
+    if (result === "You Win!") {
+        playerScore++;
+        sA.innerText = playerScore;
+    } else if (result === "It's a Tie!") {
+        return null;
+    } else {
+        computerScore++;
+        sB.innerText = computerScore; 
+    }
+    userWin();
+});
+
+btnS.addEventListener('click', () => {
+    
+    let result = playRound (`${playerSelectionArray.at(0)}` , getComputerChoice())
+    if (result === "You Win!") {
+        playerScore++;
+        sA.innerText = playerScore;
+    } else if (result === "It's a Tie!") {
+        return null;
+    } else {
+        computerScore++;
+        sB.innerText = computerScore; 
+    }
+    userWin();
+});
+
+
+
+
+
+/*function game() {
  
-    for (let i = 0; i < 5; i++) {
+    //for (let i = 0; i < 5; i++) {
         let sign = prompt("Rock, Paper or Scissors?")
             if (sign.toLowerCase() === "rock") {
                 console.log(playerSelectionArray = "rock");
@@ -40,14 +123,14 @@ function game() {
                 console.log(playerSelectionArray = "scissors");
             }
         console.log(playRound(playerSelectionArray , getComputerChoice()))
-    }
+    //}
   
-}
+}*/
 
 let playerSelectionArray = ["rock" , "paper" , "scissors"];
 
 const computerSelection = getComputerChoice();
 
 
-game();
+//game();
 
